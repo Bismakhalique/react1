97f822b846c8bc07240035b07b6e571ce6cbe346
Bismakhalique <121749134+Bismakhalique@users.noreply.github.com>
2023-09-17 11:06:07 +0500

    Add files via upload

---
+++ app2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from './component/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button title="New Button"/>
+      <h1>New React App</h1>
+      <Button title="Click Me"/>
+    </div>
+  );
+}
+
+export default App;
+
+
